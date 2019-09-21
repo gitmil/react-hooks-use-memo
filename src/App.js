0af6c468d1b1557b85e38df3fce664eb643c2574
techsithgit <hemilp@tuplecorp.com>
2019-09-21 14:20:54 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useMemo, useCallback, useEffect } from "react";
-
+import React, { useState, useMemo, useCallback } from "react";
+import Child from './components/child';
 
 import "./App.css";
-let renderCount = 0;
+
 function App() {
   const [i, setI] = useState(0);
 
@@ -11,7 +11,9 @@ function App() {
     setI(c => c + 1);
   }, [i]);
 
-
+  const memoizedChild = useMemo(()=>{
+    return <Child></Child>
+  }, []);  
 
   return (
     <div className="App">
@@ -19,8 +21,11 @@ function App() {
         <h3> Use Memo</h3>
         <h2>i: {i}</h2>
         <button onClick={callBackClick}>Increment I</button>
-
-
+        <h3> normal render</h3>
+        <Child></Child>
+        <h3> Memo render</h3>
+        {memoizedChild}
+        
       </header>
     </div>
   );
--- src/components/child.js
+++ src/components/child.js
@@ -1,12 +1,12 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 
 let renderCount = 0;
-function Child(){
-    useEffect(()=>{
-        renderCount ++;
-    })
+function Child() {
+  useEffect(() => {
+    renderCount++;
+  });
 
-    return<div>rendercount: {renderCount}</div>
+  return <div>rendercount: {renderCount}</div>;
 }
 
 export default Child;
